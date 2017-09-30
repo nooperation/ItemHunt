597edc03423033f216e307729e0a61e55cda9174
nooperation <madbrahmin@gmail.com>
2017-09-29 23:32:23 -0400

    Added HuntView, HuntView tests, added status codes to Frontend view results

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,4 +6,5 @@ app_name = 'frontend'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^register_token/(?P<token>[a-zA-Z0-9-_=]+)', views.RegisterTokenView.as_view(), name='register_token'),
+    url(r'^view_hunt/(?P<hunt_id>[0-9]+)', views.HuntView.as_view(), name='view_hunt'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,12 +1,9 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import generic
 from server.models import Hunt, HuntAuthorizationToken, AuthorizedUsers
-from server.views import json_success
 import logging
 
 
@@ -25,22 +22,35 @@ class RegisterTokenView(LoginRequiredMixin, generic.View):
             try:
                 hunt_authorization_token = HuntAuthorizationToken.objects.get(token=token)
             except HuntAuthorizationToken.DoesNotExist:
-                return render(request, 'frontend/register_token.html', {'error': 'Invalid token'})
+                return render(request, 'frontend/register_token.html', {'error': 'Invalid token'}, status=400)
 
             if hunt_authorization_token.is_expired():
-                return render(request, 'frontend/register_token.html', {'error': 'Expired token'})
+                return render(request, 'frontend/register_token.html', {'error': 'Expired token'}, status=400)
 
             hunt = hunt_authorization_token.hunt
             is_already_authorized = hunt.is_user_authorized(request.user)
             if is_already_authorized:
                 hunt_authorization_token.delete()
-                return render(request, 'frontend/register_token.html', {'error': 'User is already registered to this hunt'})
+                return render(request, 'frontend/register_token.html', {'error': 'User is already registered to this hunt'}, status=400)
 
             hunt.authorize_user(request.user)
             hunt_authorization_token.delete()
 
         except Exception as ex:
             logging.exception('Failed to register token')
-            return render(request, 'frontend/register_token.html', {'error': 'Invalid token'})
+            return render(request, 'frontend/register_token.html', {'error': 'Invalid token'}, status=400)
         # TODO: Add feedback
         return render(request, 'frontend/register_token.html', {'success': 'Server Registered', 'hunt_name': hunt.name})
+
+
+class HuntView(LoginRequiredMixin, generic.View):
+    def get(self, request, hunt_id):
+        try:
+            hunt = AuthorizedUsers.objects.get(user=request.user, hunt__id=hunt_id).hunt
+            return render(request, 'frontend/view_hunt.html', {'hunt': hunt})
+        except AuthorizedUsers.DoesNotExist:
+            logging.warning('Attempt to access unauthorized hunt. User={} hunt_id={}'.format(request.user.username, hunt_id))
+            return render(request, 'frontend/view_hunt.html', {'error': 'Invalid hunt (permissions)'}, status=403)
+        except Exception:
+            logging.exception('Failed to view hunt')
+            return render(request, 'frontend/view_hunt.html', {'error': 'Invalid hunt'}, status=403)
