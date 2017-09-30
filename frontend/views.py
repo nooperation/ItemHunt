0fd7f6552966656a9d530da5d91f04b4a0e92880
nooperation <madbrahmin@gmail.com>
2017-09-30 17:44:44 -0400

    Added breadcrumbs Moved to bootstrap 4 Overhauled ui theme

--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django import template
 
 # Create your views here.
+from django.urls import reverse
 from django.views import generic
 from server.models import Hunt, HuntAuthorizationToken, AuthorizedUsers, Item, Transaction, Player
 import logging
@@ -14,72 +15,132 @@ class IndexView(LoginRequiredMixin, generic.View):
         hunt_list = []
         for item in authenticated_services:
             hunt_list.append(item.hunt)
-        return render(request, 'frontend/index.html', {'hunt_list': hunt_list})
+
+        request.breadcrumbs = [{
+            'name': 'Home'
+        }]
+        return render(request, 'frontend/index.html', {
+            'hunt_list': hunt_list
+        })
 
 
 class RegisterTokenView(LoginRequiredMixin, generic.View):
     def get(self, request, token):
+        request.breadcrumbs = [{
+            'name': 'Home',
+            'path': reverse('frontend:index', kwargs={})
+        }]
+
         try:
             try:
                 hunt_authorization_token = HuntAuthorizationToken.objects.get(token=token)
             except HuntAuthorizationToken.DoesNotExist:
-                return render(request, 'frontend/register_token.html', {'error': 'Invalid token'}, status=400)
+                return render(request, 'frontend/register_token.html', {
+                    'error': 'Invalid token'
+                }, status=400)
 
             if hunt_authorization_token.is_expired():
-                return render(request, 'frontend/register_token.html', {'error': 'Expired token'}, status=400)
+                return render(request, 'frontend/register_token.html', {
+                    'error': 'Expired token'
+                }, status=400)
 
             hunt = hunt_authorization_token.hunt
             is_already_authorized = hunt.is_user_authorized(request.user)
             if is_already_authorized:
                 hunt_authorization_token.delete()
-                return render(request, 'frontend/register_token.html', {'error': 'User is already registered to this hunt'}, status=400)
+                return render(request, 'frontend/register_token.html', {
+                    'error': 'User is already registered to this hunt'
+                }, status=400)
 
             hunt.authorize_user(request.user)
             hunt_authorization_token.delete()
 
+            return render(request, 'frontend/register_token.html', {
+                'success': 'Server Registered',
+                'hunt_name': hunt.name
+            })
         except Exception as ex:
             logging.exception('Failed to register token')
-            return render(request, 'frontend/register_token.html', {'error': 'Invalid token'}, status=400)
-        return render(request, 'frontend/register_token.html', {'success': 'Server Registered', 'hunt_name': hunt.name})
+            return render(request, 'frontend/register_token.html', {
+                'error': 'Invalid token'
+            }, status=400)
 
 
 class HuntView(LoginRequiredMixin, generic.View):
     def get(self, request, hunt_id):
+        request.breadcrumbs = [{
+            'name': 'Home',
+            'path': reverse('frontend:index', kwargs={})
+        }]
+
         try:
             hunt = AuthorizedUsers.objects.get(user=request.user, hunt__id=hunt_id).hunt
             store_items = Item.objects.filter(type=Item.TYPE_PRIZE, hunt__id=hunt_id)
             players = Player.objects.filter(hunt__id=hunt_id)
-            return render(request, 'frontend/view_hunt.html', {'hunt': hunt, 'store_items': store_items, 'players': players})
+
+            request.breadcrumbs.append({
+                'name': hunt.name,
+            })
+            return render(request, 'frontend/view_hunt.html', {
+                'hunt': hunt,
+                'store_items': store_items,
+                'players': players
+            })
         except AuthorizedUsers.DoesNotExist:
             logging.warning('Attempt to access unauthorized hunt. User={} hunt_id={}'.format(request.user.username, hunt_id))
-            return render(request, 'frontend/view_hunt.html', {'error': 'Invalid hunt'}, status=403)
         except Exception as ex:
             logging.exception('Failed to view hunt')
-            return render(request, 'frontend/view_hunt.html', {'error': 'Invalid hunt'}, status=403)
 
+        request.breadcrumbs = []
+        return render(request, 'frontend/view_hunt.html', {
+            'error': 'Server Error'
+        }, status=403)
 
 # TODO: Test to make sure we're only getting transactions for this item's hunt
 class ItemView(LoginRequiredMixin, generic.View):
     def get(self, request, hunt_id, item_id):
+        request.breadcrumbs = [{
+            'name': 'Home',
+            'path': reverse('frontend:index', kwargs={})
+        }]
+
         try:
             hunt = AuthorizedUsers.objects.get(user=request.user, hunt__id=hunt_id).hunt
             item = Item.objects.get(pk=item_id, hunt__id=hunt_id)
             transactions = Transaction.objects.filter(item=item, hunt=hunt).select_related('player')
-            return render(request, 'frontend/view_item.html', {'item': item, 'transactions': transactions})
+
+            request.breadcrumbs.append({
+                'name': hunt,
+                'path': reverse('frontend:view_hunt', kwargs={'hunt_id': hunt_id})
+            })
+            request.breadcrumbs.append({
+                'name': item,
+            })
+            return render(request, 'frontend/view_item.html', {
+                'item': item,
+                'transactions': transactions
+            })
         except AuthorizedUsers.DoesNotExist:
             logging.warning('Attempt to access unauthorized hunt. User={} hunt_id={} item_id={}'.format(request.user.username, hunt_id, item_id))
-            return render(request, 'frontend/view_item.html', {'error': 'Server Error'}, status=403)
         except Item.DoesNotExist:
             logging.warning('Attempt to access unauthorized item. User={} hunt_id={} item_id={}'.format(request.user.username, hunt_id, item_id))
-            return render(request, 'frontend/view_item.html', {'error': 'Server Error'}, status=403)
         except Exception as ex:
             logging.exception('Failed to view item')
-            return render(request, 'frontend/view_item.html', {'error': 'Server Error'}, status=403)
+
+        request.breadcrumbs = []
+        return render(request, 'frontend/view_item.html', {
+            'error': 'Server Error'
+        }, status=403)
 
 
 # TODO: Test to make sure we're only getting transactions for this player's hunt
 class PlayerView(LoginRequiredMixin, generic.View):
     def get(self, request, hunt_id, player_id):
+        request.breadcrumbs = [{
+            'name': 'Home',
+            'path': reverse('frontend:index', kwargs={})
+        }]
+
         try:
             hunt = AuthorizedUsers.objects.get(user=request.user, hunt__id=hunt_id).hunt
             player = Player.objects.get(pk=player_id, hunt__id=hunt_id)
@@ -87,6 +148,14 @@ class PlayerView(LoginRequiredMixin, generic.View):
             total_points = player.get_total_points(hunt)
             items_purchased = [transaction for transaction in transactions if transaction.item.type == Item.TYPE_PRIZE and transaction.player == player]
             items_found = [transaction for transaction in transactions if transaction.item.type == Item.TYPE_CREDIT and transaction.player == player]
+
+            request.breadcrumbs.append({
+                'name': hunt,
+                'path': reverse('frontend:view_hunt', kwargs={'hunt_id': hunt_id})
+            })
+            request.breadcrumbs.append({
+                'name': player,
+            })
             return render(request, 'frontend/view_player.html', {
                 'player': player,
                 'total_points': total_points,
@@ -97,12 +166,12 @@ class PlayerView(LoginRequiredMixin, generic.View):
 
         except AuthorizedUsers.DoesNotExist:
             logging.warning('Attempt to access unauthorized hunt. User={} hunt_id={} player_id={}'.format(request.user.username, hunt_id, player_id))
-            return render(request, 'frontend/view_player.html', {'error': 'Server Error'}, status=403)
         except Player.DoesNotExist:
             logging.warning('Attempt to access unauthorized player. User={} hunt_id={} player_id={}'.format(request.user.username, hunt_id, player_id))
-            return render(request, 'frontend/view_player.html', {'error': 'Server Error'}, status=403)
         except Exception as ex:
             logging.exception('Failed to view item')
-            return render(request, 'frontend/view_player.html', {
-                'error': 'Server Error'
-            }, status=403)
+
+        request.breadcrumbs = []
+        return render(request, 'frontend/view_player.html', {
+            'error': 'Server Error'
+        }, status=403)
