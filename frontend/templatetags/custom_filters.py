af093aa9342f1c5d28a85c7c2d192d96558f4fb2
nooperation <madbrahmin@gmail.com>
2017-09-30 13:53:59 -0400

    Added item view and tests.

--- frontend/templatetags/custom_filters.py
+++ frontend/templatetags/custom_filters.py
@@ -1,9 +1,55 @@
 from django import template
 from datetime import date, timedelta
 
+from django.utils import html
+
 register = template.Library()
+
+
+def make_location_link_safe(region_name, x, y, z):
+    region_name_safe = html.escape(region_name)
+    x_safe = int(x)
+    y_safe = int(y)
+    z_safe = int(z)
+    return '<a href="secondlife://{}/{}/{}/{}">{}</a>'.format(
+        region_name_safe,
+        x_safe,
+        y_safe,
+        z_safe,
+        region_name_safe
+    )
 
 
 @register.filter(name='player_points')
 def player_points(player, hunt):
     return player.get_total_points(hunt)
+
+
+@register.filter(name='item_location')
+def item_location(item):
+    return make_location_link_safe(
+        item.region.name,
+        item.position_x,
+        item.position_y,
+        item.position_z,
+    )
+
+
+@register.filter(name='transaction_location')
+def transaction_location(transaction):
+    return make_location_link_safe(
+        transaction.region.name,
+        transaction.player_x,
+        transaction.player_y,
+        transaction.player_z,
+    )
+
+
+@register.filter(name='transaction_item_location')
+def transaction_location(transaction):
+    return make_location_link_safe(
+        transaction.region.name,
+        transaction.item_x,
+        transaction.item_y,
+        transaction.item_z,
+    )
---
+++ frontend/tests/views/test_item_view.py
@@ -0,0 +1,88 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from server.models import *
+
+
+class ItemViewTests(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test_password'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.client.login(username=self.username, password=self.password)
+
+        self.region = Region.objects.create(
+            name='First Region'
+        )
+        self.hunt = Hunt.objects.create(
+            name='First hunt',
+            private_token='j14WVsOPsIdzQIZGQeymFmpPv4LqpHQWck8ua0ZdCY71'
+        )
+        self.first_player = Player.objects.create(
+            uuid='41f94400-2a3e-408a-9b80-1774724f62af',
+            name='First Agent',
+            hunt=self.hunt
+        )
+        self.hunt_token = AuthorizedUsers.objects.create(
+            hunt=self.hunt,
+            user=self.user
+        )
+        self.item = Item.objects.create(
+            uuid='41f94400-2a3e-408a-9b80-1774724f62af',
+            name='First Item',
+            type=Item.TYPE_PRIZE,
+            position_x=25.00,
+            position_y=50.00,
+            position_z=75.00,
+            points=15,
+            enabled=True,
+            region=self.region,
+            hunt=self.hunt
+        )
+
+    def test_authorized(self):
+        server_data = dict(
+            hunt_id=self.hunt.id,
+            item_id=self.item.id
+        )
+
+        response = self.client.get(reverse('frontend:view_item', kwargs=server_data))
+        self.assertEquals(response.status_code, 200)
+
+    def test_not_logged_in(self):
+        server_data = dict(
+            hunt_id=self.hunt.id,
+            item_id=self.item.id
+        )
+
+        self.client.logout()
+        response = self.client.get(reverse('frontend:view_item', kwargs=server_data))
+        self.assertRedirects(response, reverse('login') + '?next=' + reverse('frontend:view_item', kwargs=server_data))
+
+    def test_unauthorized(self):
+        server_data = dict(
+            hunt_id=self.hunt.id,
+            item_id=self.item.id
+        )
+
+        AuthorizedUsers.objects.all().delete()
+        response = self.client.get(reverse('frontend:view_item', kwargs=server_data))
+        self.assertEquals(response.status_code, 403)
+
+    def test_invalid_hunt(self):
+        server_data = dict(
+            hunt_id=42,
+            item_id=self.item.id
+        )
+
+        response = self.client.get(reverse('frontend:view_item', kwargs=server_data))
+        self.assertEquals(response.status_code, 403)
+
+    def test_invalid_item(self):
+        server_data = dict(
+            hunt_id=self.hunt.id,
+            item_id=42
+        )
+
+        response = self.client.get(reverse('frontend:view_item', kwargs=server_data))
+        self.assertEquals(response.status_code, 403)
